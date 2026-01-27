b05ab6cd8d7a6b2c374077ec9d190475a8decfe7
Pratap Patra <pratappatra078@gmail.com>
2026-01-27 19:14:31 +0530

    text blink animation added

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,26 @@ hamberger.addEventListener("click",()=>{
         }, 300);
     }
 })
+
+/* blink effect*/
+const texts = ["Full Stack Developer","CS Engineer","Tech Enhusiast"];
+let count = 0;
+let index =0;
+let currText = "";
+let letter ="";
+(function type(){
+    if(count === texts.length){
+        count =0;
+    }
+    currText = texts[count];
+    letter = currText.slice(0,++index);
+    document.querySelector(".typewriter").textContent = letter;
+    if(letter.length === currText.length){
+        count++;
+        index =0;
+        setTimeout(type,2000); // wait 2 sec
+    }
+    else{
+        setTimeout(type,100); // typing speed
+    }
+})();
